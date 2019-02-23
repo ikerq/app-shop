062f61571919ea6b545e2bce8718b12de93a1824
Iker Quiros <ikerq15@gmail.com>
2019-02-23 01:57:34 -0300

    Pantalla registro y welcome, migraciones, seeds y factories de productos y categorias

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,8 +22,7 @@
 	<nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
 		<div class="container">
 			<div class="navbar-translate">
-			<a class="navbar-brand" href="https://demos.creative-tim.com/material-kit/index.html">
-				Material Kit </a>
+			<a class="navbar-brand" href="{{ url('/') }}'">App Shop</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="sr-only">Toggle navigation</span>
 				<span class="navbar-toggler-icon"></span>
@@ -33,23 +32,51 @@
 			</div>
 			<div class="collapse navbar-collapse">
 			<ul class="navbar-nav ml-auto">
-				<li class="dropdown nav-item">
+				@guest
+					<li class="nav-item">
+						<a class="nav-link" href="{{ route('login') }}">{{ __('Ingresar') }}</a>
+					</li>
+					@if (Route::has('register'))
+						<li class="nav-item">
+							<a class="nav-link" href="{{ route('register') }}">{{ __('Registro') }}</a>
+						</li>
+					@endif
+				@else
+					<li class="nav-item dropdown">
+						<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+							{{ Auth::user()->name }} <span class="caret"></span>
+						</a>
+
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="{{ route('logout') }}"
+								onclick="event.preventDefault();
+												document.getElementById('logout-form').submit();">
+								{{ __('Desconectarse') }}
+							</a>
+
+							<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+								@csrf
+							</form>
+						</div>
+					</li>
+				@endguest
+				<!-- <li class="dropdown nav-item">
 				<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
 					<i class="material-icons">apps</i> Components
 				</a>
-				<div class="dropdown-menu dropdown-with-icons">
-					<a href="../index.html" class="dropdown-item">
-					<i class="material-icons">layers</i> All Components
-					</a>
-					<a href="https://demos.creative-tim.com/material-kit/docs/2.1/getting-started/introduction.html" class="dropdown-item">
-					<i class="material-icons">content_paste</i> Documentation
-					</a>
-				</div>
+					<div class="dropdown-menu dropdown-with-icons">
+						<a href="../index.html" class="dropdown-item">
+							<i class="material-icons">layers</i> All Components
+						</a>
+						<a href="https://demos.creative-tim.com/material-kit/docs/2.1/getting-started/introduction.html" class="dropdown-item">
+							<i class="material-icons">content_paste</i> Documentation
+						</a>
+					</div>
 				</li>
 				<li class="nav-item">
-				<a class="nav-link" href="javascript:void(0)" onclick="scrollToDownload()">
-					<i class="material-icons">cloud_download</i> Download
-				</a>
+					<a class="nav-link" href="javascript:void(0)" onclick="scrollToDownload()">
+						<i class="material-icons">cloud_download</i> Download
+					</a>
 				</li>
 				<li class="nav-item">
 				<a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank" data-original-title="Follow us on Twitter">
@@ -65,7 +92,7 @@
 				<a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank" data-original-title="Follow us on Instagram">
 					<i class="fa fa-instagram"></i>
 				</a>
-				</li>
+				</li> -->
 			</ul>
 			</div>
 		</div>
