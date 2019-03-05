1cd97e67e7d2a82c5cc065df3b7c0450385083a7
Iker Quiros <ikerq15@gmail.com>
2019-03-05 09:24:37 -0300

    Se añade los factories de Category y ProductImage, también se añade el modelo del product image. Se mejora el factorie para relacionar la data de prueba.

---
+++ app/ProductImage.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductImage extends Model
+{
+    // $productImage->array_product
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use App\Product;
+use App\ProductImage;
+use App\Category;
 
 class ProductsTableSeeder extends Seeder
 {
@@ -12,6 +14,22 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Product::class, 100)->create();
+        //Crear factories simples
+        // factory(Category::class, 5)->create();
+        // factory(Product::class, 100)->create();
+        // factory(ProductImage::class, 200)->create();
+
+        //Crear factories de forma relacional
+        $categories = factory(Category::class, 5)->create();//crea las categorias y las persiste en BD
+        $categories->each(function($category){//iteramos en la categoria
+            $products = factory(Product::class, 20)->make();//Crea 20 productos pero no los persiste
+            $category->products()->saveMany($products);//asigna a cada uno de los 20 productos la categoria actual del each y los guarda en BD  
+
+            $products->each(function($p){// iteramos en los productos
+                $images = factory(ProductImage::class, 5)->make();//Crea 5 imagenes pero no las persiste 
+                $p->images()->saveMany($images);//Asigna a cada una de las 5 imagenes el producto actual del each y los guarda en BD
+            });
+        });
+
     }
 }
